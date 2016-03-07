0f5a2686363729666a880a5115d5d3e4466ccc80
mitochondrion <mitochondrion@users.noreply.github.com>
2016-03-07 01:36:30 -0500

    Adds solution for #1 Fibonacci

--- rosalind.py
+++ rosalind.py
@@ -1,17 +1,50 @@
 import sys
 
-def fibonacci(input):
-  # 1
+def fib(input):
+  '''
+  #1 Fibonacci Numbers
+  '''
   result = 0
-  previous_result = 0
+  prev = 1
 
   for i in range(input):
-    result += previous_result
+    prev, result = result, prev + result
+    #print "{}: result: {}, previous: {}".format(i, result, previous_result)
+
+  return result
   
-def fibonacci_recursive():
 
-def main(argv):
-  print 'ASDF:' + str(argv)
+def fib_r(input, prev=1, result=0):
+  '''
+  #1 Fibonacci Numbers (recursive)
+  '''
+  #print "{}: result: {}, previous: {}".format(input, prev, result)
 
-if __name__ == "__main__":
-  main(sys.argv)
+  if input <= 0:
+    return result
+
+  return fib_r(input - 1, result, prev + result)
+
+
+def fib_l(n, pad=""):
+  '''
+  #1 Fibonacci Number (abuse of recursion)
+  '''
+  print "{}l {}".format(pad, n)
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  return fib_l(n-1, pad + " ") + fib_r(n-2, pad + " ")
+
+
+def fib_r(n, pad=""):
+  print "{}r {}".format(pad, n)
+  if n == 0:
+    return 0
+  if n == 1:
+    return 1
+  return fib_l(n-1, pad + " ") + fib_r(n-2, pad + " ")
+
+
+
