fce7e8d0eb6766038d2898545a045e9dda0aca01
mitochondrion <mitochondrion@users.noreply.github.com>
2016-03-06 23:30:43 -0500

    Adds .gitignore and rosalind.py

---
+++ rosalind.py
@@ -0,0 +1,17 @@
+import sys
+
+def fibonacci(input):
+  # 1
+  result = 0
+  previous_result = 0
+
+  for i in range(input):
+    result += previous_result
+  
+def fibonacci_recursive():
+
+def main(argv):
+  print 'ASDF:' + str(argv)
+
+if __name__ == "__main__":
+  main(sys.argv)
